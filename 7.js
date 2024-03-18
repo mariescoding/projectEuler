6f5a5b5f742655cf02a2e95f10c069680a6b2974
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-18 15:02:42 +0900

    7 in progerss

---
+++ 7.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+// Q 7 : What is the 10001st prime number?
+
+// function to find prime number array 
+
+
+
+function findPrimeByIndex(num){
+    let primes = [];
+    let count = 0;
+
+    while(count<num){
+
+     
+    count++;
+    }
+
+   return primes;
+}
+
+// main code 
+
+console.log(findPrimeByIndex(10001))
+
