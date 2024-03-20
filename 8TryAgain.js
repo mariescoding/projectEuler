401f947201cdc3890372e47d16d18679da436700
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-20 19:14:59 +0900

    start optimised ver

---
+++ 8TryAgain.js
@@ -0,0 +1,24 @@
+
+// Q 8: Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+// sliding window approach 
+
+
+function getArray(num){
+    let digitArray = Array.from(String(num), Number);
+    return digitArray;
+}
+
+
+function largestAdjacentProduct( arrayCheck , n ){
+
+    // look at notion page 
+    
+
+}
+
+
+// main code 
+
+let project = getArray()
+largestAdjacentProduct(project, 13)
