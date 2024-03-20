dd8dc7f083dff4eaf539920b6ed08e6fbffc4a3f
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-20 14:54:44 +0900

    feat: function to check if prime

--- 7.js
+++ 7.js
@@ -2,9 +2,19 @@
 
 // Q 7 : What is the 10001st prime number?
 
+// function to check if a given number is a prime number 
+
+function checkPrime(checkNum){
+    let isPrime= true;
+    for (let i=2;i<checkNum;i++){
+        if(checkNum%i===0 && i!==checkNum){
+            isPrime = false;
+        }
+    }
+    return isPrime
+}
+
 // function to find prime number array 
-
-
 
 function findPrimeByIndex(num){
     let primes = [];
