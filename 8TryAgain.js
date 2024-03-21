4e2ae218485557212e7cdf9e3c933cfefdc94e19
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-21 11:38:41 +0900

    try with sliding window

--- 8TryAgain.js
+++ 8TryAgain.js
@@ -9,15 +9,34 @@ function getArray(num){
     return digitArray;
 }
 
+function arrayProduct(arrayCalc){
+    let product = 1;
+    for(let i of arrayCalc){
+        product = product * i
+    }
+    return product;
+}
 
 function largestAdjacentProduct( arrayCheck , n ){
 
     // look at notion page 
-    
+    let start = 0;
+    let end = n-1;
+    let largest = 0;
+    let currentProd = arrayProduct(arrayCheck.slice(start, end+1))
+
+    while(end<arrayCheck.length){
+        end ++ 
+        currentProd = currentProd * (arrayCheck[end]) / (arrayCheck[start])
+        if(currentProd > largest){
+            largest = currentProd;
+        }
+        start ++
+    }
+
+    return largest;
 
 }
-
-
 // main code 
 
 let project = getArray()
