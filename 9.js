81651f68e6dbe451dd08d93885e00abbbb2305f7
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-21 15:59:26 +0900

    solved q 9 brute force approach

--- 9.js
+++ 9.js
@@ -4,24 +4,40 @@
 
 // function to check if pythagoream triple , received array, returns boolean 
 
-function isPyTriple(){
+function isPyTriple(arr){
+    let a = Math.pow(arr[0],2)
+    let b = Math.pow(arr[1],2)
+    let c = Math.pow(arr[2],2)
+    return ( a + b == c)
 
 }
-
 
 // function to check is sum is n , received array, returns boolean 
 
 
-function isSum(){
-    
+function isSum(arr, n){
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+       sum+= arr[i]
+    }
+    return (sum == n)
 }
-
 
 
 // function to find subsequence from an array that meets condition, received array & integer n & condition functions (?), returns subsequence
 
 function findSubsequence(){
+    for(let a=1;a<500, a++){
+        for(let b = 2; b<500; b++){
+            for(let c = 3; c<500;c++){
+                let checkArray= [a,b,c]
+                if(isPyTriple(checkArray) && isSum(checkArray, 1000)) return a*b*c
+                
 
+            }
+
+        }
+    }
 }
 
 
