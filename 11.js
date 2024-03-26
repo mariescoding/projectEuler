e20d95bc946e2026d1c0eccf9c55dc81f26a00e0
Marie Muramatsu <muramatsumarie@gmail.com>
2024-03-26 15:31:23 +0900

    start 11

---
+++ 11.js
@@ -0,0 +1,13 @@
+
+//  Q11; What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20x20 grid?
+
+// store grid data in matrix
+
+
+// func to find subarray horizontally 
+
+
+// func to find subarray veritcally 
+
+
+// func to find subarray diagonally 
